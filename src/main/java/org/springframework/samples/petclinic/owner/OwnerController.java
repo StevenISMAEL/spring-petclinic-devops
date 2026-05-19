eba88ac2037ce0ca68ee383fe18de30e87156b9f
stevenISMAEL <larasteveen72@gmail.com>
2026-05-18 22:49:35 -0500

    fix: remove SQL injection vulnerability

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -174,9 +174,9 @@ class OwnerController {
 	}
 	// Vulnerabilidad deliberada: SQL Injection
 
-	public void buscarOwner(String nombre) {
-		String query = "SELECT * FROM owners WHERE nombre = '" + nombre + "'";
-		System.out.println(query);
-	}
+	// public void buscarOwner(String nombre) {
+	// 	String query = "SELECT * FROM owners WHERE nombre = '" + nombre + "'";
+	// 	System.out.println(query);
+	// }
 
 }
